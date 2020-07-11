c40b701dbe308cd1da8c4095974ab1afdb570187
Gustavo Garcia <gcastilhos@gmail.com>
2020-07-11 18:19:04 +1000

    Added mock data js

---
+++ src/mock_data.js
@@ -0,0 +1,41 @@
+
+const mock_data = {
+  columns: [
+    'Event_Id',
+    'Date',
+    'Time_24H',
+    'Duration_Min',
+    'Appl_Id',
+    'Appl.NAME',
+    'Global_active_power',
+    'Global_reactive_power',
+    'Voltage',
+    'Global_intensity',
+    'Total Subm. 1+2+3',
+    'Total measum. WH',
+    'POWER Composition [kWH]',
+    'USE CATEG',
+    'CATEGORY NAME (Literal)'
+  ],
+  data: [
+    [
+      40979,
+      '11/07/2020',
+      '18:49:45',
+      2,
+      '0006_c2',
+      'PC1_Gar',
+      2.69,
+      0.09,
+      248.75,
+      11.45,
+      22,
+      44,
+      0.04,
+      'E',
+      'EDUCATION, COMMS, ENTERNTAINMENT'
+    ]
+  ]
+};
+
+export default mock_data;
