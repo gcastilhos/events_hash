d67d7eb79d1004139c9553c2241000f2e3e63258
Gustavo Garcia <gcastilhos@gmail.com>
2020-07-23 11:19:22 +1000

    Updated default landing page to index.html (from events.html)

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   chainWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
       config.plugin('html').tap((opts) => {
-        opts[0].filename = './events.html';
+        opts[0].filename = './index.html';
         return opts;
       });
     }
