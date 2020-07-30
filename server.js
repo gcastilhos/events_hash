cff714a5f7e1a3539d88128d5257c9222bff045d
Gustavo Garcia <gcastilhos@gmail.com>
2020-07-30 13:00:10 +1000

    Refactored server.js

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
-const express = require('express');
+const express = require('express')
 const serveStatic = require("serve-static")
-const path = require('path');
-const fetch = require('node-fetch');
-const app = express();
+const path = require('path')
+const fetch = require('node-fetch')
+const app = express()
 
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
-const port = process.env.PORT || 80;
-const DATA_URI = process.env.DATA_URI || 'https://eventqueue.herokuapp.com/events';
+const port = process.env.PORT || 80
+const DATA_URI = process.env.DATA_URI || 'https://eventqueue.herokuapp.com/events'
 
 app.get('/events', (req, res) => {
-  console.log('Events requested');
-  var jsonResponse;
-  fetch(DATA_URI, {method: 'GET'}).
-    then(res => res.json()).
-    then((json) => {
-      console.log(json);
-      res.send(json);
-    });
-});
+  console.log('Events requested')
+  console.log(`DATA_URI: ${DATA_URI}`) 
+  fetch(DATA_URI, {method: 'GET'})
+    .then(response => response.json())
+    .then(json => res.send(json))
+})
 
-app.listen(port);
-console.log("Server started at port " + port);
+app.listen(port)
+console.log("Server started at port " + port)
